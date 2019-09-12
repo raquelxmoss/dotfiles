a08dfbb471d53f6fefaaabc36633b5c150835413
Raquel Moss <raquelxmoss@gmail.com>
2019-09-12 17:44:36 +0200

    self-control for 8 hours

--- .selfcontrol.sh
+++ .selfcontrol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-defaults write org.eyebeam.SelfControl BlockDuration -int 3
+defaults write org.eyebeam.SelfControl BlockDuration -int 480
 defaults write org.eyebeam.SelfControl HostBlacklist -array twitter.com tweetdeck.twitter.com reddit.com
 defaults read org.eyebeam.SelfControl
 sudo /Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl $(id -u $(whoami)) --install
