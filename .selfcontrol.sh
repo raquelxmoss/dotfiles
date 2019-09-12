1334ae4a098b84d5b4815a8780bb865525ea1a38
Raquel Moss <raquelxmoss@gmail.com>
2019-09-12 17:56:56 +0200

    Add usage instructions to selfcontrol autoscheduler

--- .selfcontrol.sh
+++ .selfcontrol.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# to use this to automatically schedule SelfControl to turn on every day,
+# save this script to /usr/local/bin and make it executable
+# then open up `sudo crontab -e` and add something like this:
+# 0 8 * * * /usr/local/bin/.selfcontrol.sh
 
 defaults write org.eyebeam.SelfControl BlockDuration -int 480
 defaults write org.eyebeam.SelfControl HostBlacklist -array twitter.com tweetdeck.twitter.com reddit.com
